1666c9ab575751e26b05adbe1d735d127c3fe20c
Rajkumar <solomonrajkumar.as@gmail.com>
2017-03-30 21:37:09 +0530

    Ïmplemented add operation for calculator using Stack datastructure

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -12,7 +12,15 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var btnSound: AVAudioPlayer!
+    
+    var numberBeingEntered = ""
+    
+    var calculatorStack = ["", ""]
+    
+    var result = 0
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -48,9 +56,56 @@ class ViewController: UIViewController {
         
     }
     
+    
+    
+    @IBAction func equalsButtonPressed(_ sender: Any) {
+        
+        playSound()
+        
+        result = Int(calculatorStack[0])! + Int(numberBeingEntered)!
+        
+        print(result)
+        
+        outputLabel.text = "\(result)"
+        calculatorStack[0] = "\(result)"
+        calculatorStack[1] = ""
+    }
+    
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        playSound()
+        
+        if (calculatorStack[0] == "") {
+            
+            calculatorStack[0] = numberBeingEntered
+            
+        }
+        
+        
+        if (calculatorStack[1] == "") {
+
+            calculatorStack[1] = "+"
+            
+        } else {
+            
+            calculatorStack[0] = "\(Int(calculatorStack[0])! + Int(numberBeingEntered)!)"
+            outputLabel.text = calculatorStack[0]
+            
+        }
+        
+        numberBeingEntered = ""
+        
+    }
+    
+    
     @IBAction func numberPressed(sender: UIButton) {
         
         playSound()
+        
+        numberBeingEntered += "\(sender.tag)"
+        
+        outputLabel.text = numberBeingEntered
         
     }
 
